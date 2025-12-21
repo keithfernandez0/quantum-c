f545439250bd0d412feba06bce99fc5cb4dfd453
Keith Fernandez <keith-developer@proton.me>
2025-12-20 23:26:25 -0500

    verified make files compiled and environment doesn't crash

---
+++ build/CMakeFiles/quantumc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quantumc.dir/link.d"
+  "CMakeFiles/quantumc.dir/src/qc_session.c.o"
+  "CMakeFiles/quantumc.dir/src/qc_session.c.o.d"
+  "libquantumc.pdb"
+  "libquantumc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quantumc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
